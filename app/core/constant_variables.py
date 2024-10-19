cccc46eea839f6cba29582c165e2435c8a643a3b
mr.khavari <mr.khavari.n@gmail.com>
2024-10-19 22:57:39 +0330

    add admin accessibility decorator

--- app/core/constant_variables.py
+++ app/core/constant_variables.py
@@ -4,3 +4,4 @@ ADMIN_USERNAME_MAX_LENGTH = 32
 ### MESSAGES ###
 INCORRECT_USERNAME_OR_PASSWORD = "Incorrect Username Or Password"
 ADMIN_NOT_FOUND = "Admin Not Found"
+ADMIN_ACCESS_DENIED = "You Have Not Access To This Route"
