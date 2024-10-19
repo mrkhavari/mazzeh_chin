288b1c4b39e723ff0c495923733e8f1182b3ca22
mr.khavari <mr.khavari.n@gmail.com>
2024-10-19 21:25:52 +0330

    add get admin by username function to amin dal file

--- app/admin/dals/admin_dal.py
+++ app/admin/dals/admin_dal.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from app.util.crud.base import CRUDBase
 from app.admin.models.admin import Admin
 
@@ -5,3 +7,10 @@ from app.admin.models.admin import Admin
 class AdminDAL(CRUDBase[Admin]):
     def __init__(self):
         super().__init__(model=Admin)
+
+    async def get_by_username(
+        self,
+        username: str,
+    ) -> Optional[Admin]:
+        admin: Optional[Admin] = Admin.nodes.get_or_none(username=username)
+        return admin
